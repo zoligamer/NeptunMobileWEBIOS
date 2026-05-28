f2d044c5aaaea0f65d8440344d024e04eb544521
zoligamer <kuruczzoltan2006@gmail.com>
2026-05-28 02:10:15 +0200

    a

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "725613958" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3698441577" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
